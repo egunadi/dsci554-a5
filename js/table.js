8e61bcf0873adad64aab6c8fd5f2a8fc6f1c69f3
ebenGunadi <egunadi@medinformatix.com>
2023-09-26 10:29:50 -0700

    Add py/table.py

--- js/table.js
+++ js/table.js
@@ -1,13 +1,7 @@
 // Create the table
 function createTable() {
-    // Sample data for the table
-    var tableData = [
-        { Name: "Item 1", Value: 10 },
-        { Name: "Item 2", Value: 20 },
-        { Name: "Item 3", Value: 15 },
-        { Name: "Item 4", Value: 25 },
-        { Name: "Item 5", Value: 30 }
-    ];
+    var tableData = 
+        [{"Attrib":"population_count","Year":151.0,"Japan":151.0,"China":151.0},{"Attrib":"population_mean","Year":2025.0,"Japan":105279.5585364239,"China":1100453.0504569535},{"Attrib":"population_std","Year":43.7340447097,"Japan":17333.8903876381,"China":258957.4247522522},{"Attrib":"population_min","Year":1950.0,"Japan":73644.064,"China":543979.233},{"Attrib":"population_25%","Year":1987.5,"Japan":90345.8875,"China":897163.946},{"Attrib":"population_50%","Year":2025.0,"Japan":106674.353,"China":1144050.9169999999},{"Attrib":"population_75%","Year":2062.5,"Japan":122114.6465,"China":1337694.2515},{"Attrib":"population_max","Year":2100.0,"Japan":128117.042,"China":1425893.4650000001},{"Attrib":"gdp_count","Year":52.0,"Japan":52.0,"China":52.0},{"Attrib":"gdp_mean","Year":1995.5,"Japan":413399545733801.9375,"China":22394941923076.921875},{"Attrib":"gdp_std","Year":15.1547572289,"Japan":155536388068634.03125,"China":32029974234146.58984375},{"Attrib":"gdp_min","Year":1970.0,"Japan":76539307651500.0,"China":227970000000.0},{"Attrib":"gdp_25%","Year":1982.75,"Japan":292123425000000.0,"China":585902500000.0},{"Attrib":"gdp_50%","Year":1995.5,"Japan":498961800000000.0,"China":6657675000000.0},{"Attrib":"gdp_75%","Year":2008.25,"Japan":533179250000000.0,"China":32656325000000.0},{"Attrib":"gdp_max","Year":2021.0,"Japan":558491200000000.0,"China":114366970000000.0}];
 
     // Select the table container
     var tableContainer = d3.select("#table-container");
@@ -20,7 +14,7 @@ function createTable() {
     // Append table header
     thead.append("tr")
         .selectAll("th")
-        .data(["Name", "Value"])
+        .data(["Attrib", "Year", "Japan", "China"])
         .enter()
         .append("th")
         .text(function (column) { return column; });
@@ -34,7 +28,7 @@ function createTable() {
     // Append data cells within rows
     var cells = rows.selectAll("td")
         .data(function (row) {
-            return ["Name", "Value"].map(function (column) {
+            return ["Attrib", "Year", "Japan", "China"].map(function (column) {
                 return { column: column, value: row[column] };
             });
         })
--- py/table.py
+++ py/table.py
@@ -1,7 +1,56 @@
 import pandas as pd
 
-population_filepath = '../data/population.original.csv'
-gdp_filepath = '../data/gdp.original.csv'
+def export_statistics():
+    population_filepath = '../data/population.original.csv'
+    gdp_filepath = '../data/gdp.original.csv'
+    
+    population_df = pd.read_csv(population_filepath, delimiter=',', encoding='utf-8')
+    gdp_df = pd.read_csv(gdp_filepath, delimiter=',', encoding='utf-8')
+    
+    population_df_japan = population_df[(population_df['Country or Area'] == 'Japan') & 
+                                        (population_df['Variant'] == 'Medium')]
+    
+    population_df_china = population_df[(population_df['Country or Area'] == 'China') & 
+                                        (population_df['Variant'] == 'Medium')]
+    
+    gdp_df_japan = gdp_df[(gdp_df['Country or Area'] == 'Japan') & 
+                            (gdp_df['Item'] == 'Gross Domestic Product (GDP)')]
+    
+    gdp_df_china = gdp_df[(gdp_df['Country or Area'] == 'China (mainland)') & 
+                            (gdp_df['Item'] == 'Gross Domestic Product (GDP)')]
+    
+    population_df_japan = population_df_japan[['Year(s)', 'Value']]
+    population_df_japan = population_df_japan.rename(columns={'Value': 'Japan'})
+    
+    population_df_china = population_df_china[['Year(s)', 'Value']]
+    population_df_china = population_df_china.rename(columns={'Value': 'China'})
+    
+    population_df = population_df_japan.merge(population_df_china, on='Year(s)')
+    population_df = population_df.rename(columns={'Year(s)': 'Year'})
+    
+    gdp_df_japan = gdp_df_japan[['Year', 'Value']]
+    gdp_df_japan = gdp_df_japan.rename(columns={'Value': 'Japan'})
+    
+    gdp_df_china = gdp_df_china[['Year', 'Value']]
+    gdp_df_china = gdp_df_china.rename(columns={'Value': 'China'})
+    
+    gdp_df = gdp_df_japan.merge(gdp_df_china, on='Year')
+    
+    population_statistics_df = population_df.describe() \
+                                            .reset_index() \
+                                            .rename(columns={'index': 'Attrib'})
+                                            
+    population_statistics_df['Attrib'] = 'population_' + population_statistics_df['Attrib']              
+    
+    gdp_statistics_df = gdp_df.describe() \
+                              .reset_index() \
+                              .rename(columns={'index': 'Attrib'})
+                              
+    gdp_statistics_df['Attrib'] = 'gdp_' + gdp_statistics_df['Attrib']
+    
+    statistics_df = pd.concat([population_statistics_df, gdp_statistics_df])
+    
+    statistics_df.to_json('../data/statistics.json', orient='records')
 
-population_df = pd.read_csv(population_filepath, delimiter=',', encoding='utf-8')
-gdp_df = pd.read_csv(gdp_filepath, delimiter=',', encoding='utf-8')
+if __name__ == '__main__':
+    export_statistics()
